5bd9823af18ba2f7cfb8545efffaeb428edfc709
Pierre Ollivier <pierreollivier@MacBook-Pro-de-Pierre.local>
2022-11-23 13:20:20 +0100

    Rename 'logimagewindow' into 'nonogramwindow', fix old Logimage imports

--- nonogramwindow.py
+++ nonogramwindow.py
@@ -1,16 +1,16 @@
 from utils import list_to_horizontal_str, list_to_vertical_str
-from logimage import Logimage
+from nonogram import Nonogram
 import tkinter as tk
 
 
-def create_window(logimage: Logimage):
+def create_window(nonogram: Nonogram):
     win = tk.Tk()
-    MAX_HEIGHT = 600  # 500 for the logimage, 100 for the constraints
-    MAX_WIDTH = 600  # 500 for the logimage, 100 for the constraints
-    LOG_WIDTH = logimage.width
-    LOG_HEIGHT = logimage.height
+    MAX_HEIGHT = 600  # 500 for the nonogram, 100 for the constraints
+    MAX_WIDTH = 600  # 500 for the nonogram, 100 for the constraints
+    LOG_WIDTH = nonogram.width
+    LOG_HEIGHT = nonogram.height
 
-    # TODO: add support for non-squared logimages and adjust the place taken by the constraints
+    # TODO: add support for non-squared nonograms and adjust the place taken by the constraints
 
     canvas = tk.Canvas(win, width=MAX_WIDTH + 1, height=MAX_HEIGHT + 1)
     canvas.create_line(0, 100, MAX_WIDTH, 100, width=2)
@@ -27,7 +27,7 @@ def create_window(logimage: Logimage):
             canvas.create_line(100 + (1 + vline_index) * 500/LOG_WIDTH,
                                0, 100 + (1 + vline_index) * 500/LOG_WIDTH, 600)
         canvas.create_text(100 + (0.5 + vline_index) * 500 /
-                           LOG_WIDTH, 15, text=list_to_vertical_str(logimage.top_constraints[vline_index]), anchor="n")
+                           LOG_WIDTH, 15, text=list_to_vertical_str(nonogram.top_constraints[vline_index]), anchor="n")
 
     for hline_index in range(LOG_HEIGHT):
         if hline_index % 5 == 4:
@@ -37,5 +37,5 @@ def create_window(logimage: Logimage):
             canvas.create_line(0, 100 + (1 + hline_index) * 500 /
                                LOG_HEIGHT, 600, 100 + (1 + hline_index) * 500/LOG_HEIGHT)
         canvas.create_text(15, 100 + (0.5 + hline_index) * 500 /
-                           LOG_HEIGHT, text=list_to_horizontal_str(logimage.left_constraints[hline_index]), anchor="w")
+                           LOG_HEIGHT, text=list_to_horizontal_str(nonogram.left_constraints[hline_index]), anchor="w")
     canvas.pack()
