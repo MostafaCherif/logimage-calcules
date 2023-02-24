14e6efb76e9764c447c71e1c4c9368ad3aa0d802
Pierre Ollivier <pierre.ollivier2001@gmail.com>
2023-02-24 12:19:32 +0100

    Add new exceptions for nonogram solving handling

--- exceptions.py
+++ exceptions.py
@@ -1,3 +1,13 @@
 class NoSolutionError(Exception):
-    """Raised when a `Board`, even incomplete, cannot be the solution of a `Logimage`"""
+    """Raised when a `Board`, even incomplete, cannot be the solution of a `Nonogram`."""
+    pass
+
+
+class NullNonogramError(Exception):
+    """Raised while trying to solve a `Nonogram` being `None`."""
+    pass
+
+
+class MultipleSolutionsError(Exception):
+    """Raised when trying to solve a `Nonogram` that has multiple solutions."""
     pass
